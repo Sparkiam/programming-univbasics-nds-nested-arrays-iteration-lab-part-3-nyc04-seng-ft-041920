70676f4aad9801c22930ebaaa78dbc02954f5f17
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-06 19:39:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,7 +14,6 @@ def join_nested_strings(mixed_data_1)
   array = mixed_data_1.flatten
   array.delete_if {|x| x.is_a? Integer}
   return array
-  end
 end  
 #   if mixed_data_1 === Class
 # array.each do |x| 
