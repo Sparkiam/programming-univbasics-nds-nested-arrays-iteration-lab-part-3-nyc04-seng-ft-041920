ed7561296063b7863888f96d8c2c9cfd5a862bfd
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-06 19:38:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,34 @@
-def join_nested_strings(src)
-  # src will be an Array of Arrays of Strings and Integers
-  # Combine all Strings present in the AoA into a single value and return it
-end
+# mixed_data_1 = [
+#   ["The", 4, "quick"],
+#   [-1, "brown", "fox", 30],
+#   ["studied", 101, 233, "Ruby"]
+# ]
+
+# mixed_data_2 = [
+#   ["Turning", "and", "turning", "in", "the", "widening", "gyre"],
+#   ["The", "falcon", "cannot", "hear", "the", "falconer;"],
+#   ["Things", "fall", "apart;", "the", "centre", "cannot", "hold"]
+# ]
+
+def join_nested_strings(mixed_data_1)
+def join_nested_strings(mixed_data_1)
+  array = mixed_data_1.flatten
+  array.delete_if {|x| x.is_a? Integer}     return array end 
+#   if mixed_data_1 === Class
+# array.each do |x| 
+#   if x === String(" ")
+#     p x
+#     end
+# end
+
+# mixed_data_1 = [] 
+# array.each do |x| 
+# # array.delete_if {|x| x.is_a? Integer}
+# end 
+# end 
+
+# mixed_data_1.join(" ")
+
+
+
+end 
